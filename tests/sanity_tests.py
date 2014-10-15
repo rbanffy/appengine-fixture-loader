008b6d9a1598e0c2e7cb6de1ab2aa8bd9d710970
Ricardo Bánffy <rbanffy@gmail.com>
2014-10-15 01:11:55 -0300

    There has to be a better way. Mock, perhaps?

--- tests/sanity_tests.py
+++ tests/sanity_tests.py
@@ -1,1 +1,4 @@
-from appengine_fixture_loader.loader import *
+#from appengine_fixture_loader.loader import load_fixture
+
+# TODO: We need a way to test it without relying on Google App Engine SDK being
+# installed on this envidonment
