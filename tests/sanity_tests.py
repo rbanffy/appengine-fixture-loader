e38c3bee641caa6ab4d8e0dc3e57ac42b0c8bb9e
Ricardo Bánffy <rbanffy@gmail.com>
2014-10-15 22:14:41 -0300

    Make import without Google App Engine SDK fail with a custom message. Ensure in tests it does so.

--- tests/sanity_tests.py
+++ tests/sanity_tests.py
@@ -2,3 +2,15 @@
 
 # TODO: We need a way to test it without relying on Google App Engine SDK being
 # installed on this envidonment
+
+import unittest
+
+class SanityTest(unittest.TestCase):
+
+    def testImportFailure(self):
+        "This test will fail because Google's SDK is not present"
+
+        def _dummy():
+            from appengine_fixture_loader.loader import load_fixture
+
+        self.assertRaises(ImportError, _dummy)
