94adfcee938d3b13e42641fbe42de4407797e697
Ricardo Bánffy <rbanffy@gmail.com>
2014-11-08 12:33:31 -0200

    Rebuild based on cookiecutter cookiecutter-pypackage template

--- setup.py
+++ setup.py
@@ -1,32 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from codecs import open
-from setuptools import setup
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
 
 setup(
     name='Appengine-Fixture-Loader',
-    version='0.1',
-    url='http://github.com/rbanffy/appengine-fixture-loader/',
-    license='Apache',
+    version='0.1.0',
+    description='Appengine fixture loader',
+    long_description=readme + '\n\n' + history,
     author='Ricardo Bánffy',
     author_email='appengine-fixture-loader@autonomic.com.br',
-    description='Appengine fixture loader',
-    long_description=open('README.md', 'r', 'utf8').read(),
-    keywords=['appengine', 'loader', 'fixture'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
+    url='http://github.com/rbanffy/appengine-fixture-loader/',
     packages=[
         'appengine_fixture_loader',
     ],
-    package_data={},
-    install_requires=[],
-    extras_require={}
+    package_dir={'appengine_fixture_loader':
+                 'appengine_fixture_loader'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache",
+    zip_safe=False,
+    keywords=['appengine', 'loader', 'fixture'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
